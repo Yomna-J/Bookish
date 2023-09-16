994453278254e8056935e75419b9c212d91e0509
Yomna-J <youmnajaza@gmail.com>
2023-09-16 17:37:22 +0300

    feat: create logout routes     - create logout controller     - add credentials for cors

--- server/server.js
+++ server/server.js
@@ -4,19 +4,22 @@ const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const verifyJWT = require("./middleware/verifyJWT");
 const cookieParser = require("cookie-parser");
+const credentials = require("./middleware/credentials");
 
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(credentials);
 app.use(cors(corsOptions));
 app.use(cookieParser());
 
-// routes
+// Routes
 app.use("/register", require("./routes/register"));
 app.use("/login", require("./routes/login"));
 app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
 
 app.use(verifyJWT);
 app.use("/user", require("./routes/user"));
@@ -25,7 +28,6 @@ app.use((req, res) => {
   res.status(404).json({ error: "Not Found" });
 });
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
