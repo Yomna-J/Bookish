61d0c03ee93f5c266409655b46dc1bcc4c182f91
Yomna-J <youmnajaza@gmail.com>
2023-12-02 16:18:39 +0300

    chore: update checkout for non-signed in users

--- client/src/pages/Cart.tsx
+++ client/src/pages/Cart.tsx
@@ -54,7 +54,7 @@ const Cart = () => {
         console.log("Error: Unable to create checkout session");
       }
     } catch (error) {
-      navigate("/login");
+      navigate("/login", { state: { from: location }, replace: true });
     }
   };
 
