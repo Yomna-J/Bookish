8752204be0b02edebbb7929b04790ac1bf04c9e4
Yomna-J <youmnajaza@gmail.com>
2023-12-02 16:24:35 +0300

    chore: update checkout success page

--- client/src/pages/CheckoutSuccess.tsx
+++ client/src/pages/CheckoutSuccess.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { axiosPrivate } from "../api/axios";
 import { useNavigate } from "react-router-dom";
 import { useCart } from "react-use-cart";
+import success from "../assets/success.svg";
 
 const CheckoutSuccess = () => {
   const [isPaid, setIsPaid] = useState(false);
@@ -30,9 +31,9 @@ const CheckoutSuccess = () => {
     <div>
       {isPaid && (
         <div className="mx-auto flex h-[80vh] flex-col items-center justify-center px-4 md:px-0 lg:max-w-7xl">
-          <img src="" alt="empty cart" className="md:w-1/2" />
+          <img src={success} alt="empty cart" className="md:w-1/2" />
           <h1 className="mb-3 text-2xl font-bold">
-            Your order has been placed
+            Your order has been placed successfully
           </h1>
           <button
             className="w-44 rounded-full bg-primary p-2 text-lg font-bold text-white hover:bg-darkPrimary"
